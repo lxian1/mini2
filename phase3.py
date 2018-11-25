9fca3b15b19dd69314ee079a57f4b4e06df9c19e
Linhao Xian <lxian1@ualberta.ca>
2018-11-25 13:46:57 -0700

    rename

--- phase3.py
+++ phase3.py
@@ -1,12 +1,6 @@
 from bsddb3 import db
 
 def main():
-    pairs = []
-    with open('ads.txt','r') as file:
-        d = file.readlines()
-        for line in d:
-            pairs.append(line.split(':'))
-        file.close()
     database = db.DB()
     DB_File = "project2.db"
     database.open(DB_File ,None, db.DB_HASH, db.DB_CREATE)
