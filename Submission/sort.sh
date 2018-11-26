995c0ed0db21cea8a72354d6a9e0a9eb6e38b4c7
SeiverB <43014287+SeiverB@users.noreply.github.com>
2018-11-26 16:48:50 -0700

    tt

---
+++ Submission/sort.sh
@@ -0,0 +1,10 @@
+#Sorts the files within ads.txt, terms.txt, pdates.txt, and prices.txt,
+#According to ther key type (Lexicographic sort: pdates/terms; Numerical sort: ads/prices)
+#Then removes duplicate lines, prepares the file with external perl script break.pl
+#Which replaces delimiters with line breaks, and removes backslashes.
+#Using db_load the files are then convereted into Berkeley DB files ready for use.
+#The files are organized using hashes (ads) and btrees (terms, pdates, prices)
+sort ads.txt -n | uniq | perl break.pl | db_load -T -t hash ad.idx
+sort terms.txt | uniq | perl break.pl | db_load -T -t btree te.idx
+sort pdates.txt | uniq | perl break.pl | db_load -T -t btree da.idx
+sort prices.txt -n | uniq | perl break.pl | db_load -T -t btree pr.idx
