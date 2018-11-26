ca6eaeac569ec973eba8cf861ede8c762ec4f88e
Linhao Xian <lxian1@ualberta.ca>
2018-11-26 16:28:56 -0700

    zip

--- PHASE1.py
+++ PHASE1.py
@@ -13,43 +13,46 @@ def get_content(file_name,tag,slash_tag):
         file.close()
     return container
 
-def write_terms():
+#This function will generate terms.txt
+def write_terms(file_name):
     terms = []
     terms2 = []
     terms3 = []
     terms_final = []
     #Take ids,titles and descriptions from xml 
-    ids = get_content('1k.txt','<aid>','</aid>')
-    content = get_content('1k.txt','<ti>','</desc>')
-    #remove strange expressions
+    ids = get_content(file_name,'<aid>','</aid>')
+    content = get_content(file_name,'<ti>','</desc>')
+    #remove the tags from the content
     for i in range(len(content)):
         terms.append(content[i].replace("</ti><desc>"," ").lower())
+    #remove the puntuation markers
     for i in range(len(content)):
         terms2.append(re.sub(r'[?|$|.|!|,|-|-|]',r'',terms[i]))
+    #remove the weird expressions
     for i in range(len(content)):
         terms3.append(re.sub(r'[^a-zA-Z0-9\._-]',r' ',terms2[i]))
+    #remove the words less than 3 characters
     for i in range(len(content)):
         terms_final.append(' '.join(word for word in terms3[i].split() if len(word)>2))
     dictionary = dict(zip(terms_final,ids))
 
     #Wrtie the temrs into a new file
     file = open('terms.txt','w')
-    #file.writelines(["%s\n" % item  for item in terms_final])
-    #file.writelines(["%s\n" % item  for item in ids])
     for k, v in dictionary.items():
         for item in k.split(" "):
-            file.write(str(item) + ':'+ str(v) + '\n' )
-         
+            file.write(str(item) + ':'+ str(v) + '\n')
+    file.close()
 
-def write_pdates():
+#This function will generate pdates.txt
+def write_pdates(file_name):
     write_final = []
     d = []
     f = []
     #take the data from xml
-    ids = get_content('1k.txt','<aid>','</aid>')
-    dates = get_content('1k.txt','<date>','</date>')
-    cats = get_content('1k.txt','<cat>','</cat>')
-    locs = get_content('1k.txt','<loc>','</loc>')
+    ids = get_content(file_name,'<aid>','</aid>')
+    dates = get_content(file_name,'<date>','</date>')
+    cats = get_content(file_name,'<cat>','</cat>')
+    locs = get_content(file_name,'<loc>','</loc>')
     #format the data
     write = zip(ids,cats,locs)
     write = list(write)
@@ -63,24 +66,25 @@ def write_pdates():
     file = open('pdates.txt','w')
     file.writelines(["%s\n" % item  for item in f])
     file.close()
-
-def write_ads():
-    ads = get_content('10.txt','<ad>','</ad>')
-    ids = get_content("10.txt", "<aid>", "</aid>")
-    file = open("ads.txt", "w") # TODO: Detect malformed ads? (missing an ad ID) len(ads) != len(ids)
+    
+#This function will generate ads.txt
+def write_ads(file_name):
+    ads = get_content(file_name,'<ad>','</ad>')
+    ids = get_content(file_name, "<aid>", "</aid>")
+    file = open("ads.txt", "w") 
     for i in range(len(ads)):
-        file.write("{}:<ad>{}</ad>\n".format(ids[i], ads[i])) # TODO Shouldn't have to replace ad tags
-    file.close()
+        file.write("{}:<ad>{}</ad>\n".format(ids[i], ads[i]))
 
-def write_prices():
+#This function will generate prices.txt
+def write_prices(file_name):
     write_final = []
     d = []
     f = []
     #take the data from xml
-    prices = get_content('1k.txt','<price>','</price>')
-    ids = get_content('1k.txt','<aid>','</aid>')
-    cats = get_content('1k.txt','<cat>','</cat>')
-    locs = get_content('1k.txt','<loc>','</loc>')    
+    prices = get_content(file_name,'<price>','</price>')
+    ids = get_content(file_name,'<aid>','</aid>')
+    cats = get_content(file_name,'<cat>','</cat>')
+    locs = get_content(file_name,'<loc>','</loc>')    
     write = zip(ids,cats,locs)
     write = list(write)
     for i in range(len(write)):
@@ -94,9 +98,11 @@ def write_prices():
     file.writelines(["%s\n" % item  for item in f])
     file.close()    
 
+
 def main():
-    write_ads()
-    write_pdates()
-    write_terms()
-    write_prices()
+    file_name = input('Please enter the file name: ') #Take the xml file name as input
+    write_terms(file_name)
+    write_pdates(file_name)
+    write_ads(file_name)
+    write_prices(file_name)   
 main()
